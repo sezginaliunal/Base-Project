1eb792c20d8b753f4cabd0b7f0bea45d0d36adcd
Your Name <your_email@example.com>
2024-08-29 13:01:06 +0300

    Updated

--- lib/core/services/api/base_network.dart
+++ lib/core/services/api/base_network.dart
@@ -6,7 +6,11 @@ import 'package:test_project/core/controllers/network_controller.dart';
 =======
 
 import 'package:started_app/core/config/constants/api_urls.dart';
+<<<<<<< HEAD
 import 'package:started_app/core/controllers/network_controller.dart';
+>>>>>>> Updated
+=======
+import 'package:started_app/core/services/network/network_service.dart';
 >>>>>>> Updated
 import 'package:vexana/vexana.dart';
 
